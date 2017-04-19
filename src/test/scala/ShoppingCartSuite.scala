eba6a40533a851b69d9fcddfba21ba46e73ac55b
kamineni15 <kamineni.cse@gmail.com>
2017-04-19 17:31:51 +0100

    Amended Test - Included Offers - Should Fail (minimal code next step)

--- src/test/scala/ShoppingCartSuite.scala
+++ src/test/scala/ShoppingCartSuite.scala
@@ -5,45 +5,98 @@
 import org.scalatest.{Matchers, GivenWhenThen, FeatureSpec}
 import com.hmrc.shoppingcart._
 
-class ShoppingCartWithNoOffers extends FeatureSpec with GivenWhenThen with Matchers
+class ShoppingCartSuite extends FeatureSpec with GivenWhenThen with Matchers
 {
-  feature ("Basic Shopping Cart"){
+  feature ("Basic Shopping Cart") {
 
-    scenario("Testing Checkout with 1 apple and no offer"){
+    scenario("Testing Checkout with 1 apple and no offer") {
 
       Given("A shopping cart")
-      val shoppingCart =  ShoppingCart
+      val shoppingCart = ShoppingCart
 
       When("Add products with no offers")
       val items = Array("apple")
 
       Then("Total sum should be simple sum of prices")
-      shoppingCart.checkout(items) should be (0.60)
+      shoppingCart.checkout(items) should be(0.60)
     }
 
-    scenario("Testing Checkout with 1 orange and no offer"){
+    scenario("Testing Checkout with 1 orange and no offer") {
 
       Given("A shopping cart")
-      val shoppingCart =  ShoppingCart
+      val shoppingCart = ShoppingCart
 
       When("Add one orange")
       val items = Array("orange");
 
       Then("Total sum should be price of one orange")
-      shoppingCart.checkout(items) should be (0.25)
+      shoppingCart.checkout(items) should be(0.25)
     }
 
-    scenario("Testing Checkout with no offer and multiple items"){
+    scenario("Testing Checkout with no offer and multiple items") {
 
       Given("A shopping cart")
-      val shoppingCart =  ShoppingCart
+      val shoppingCart = ShoppingCart
 
       When("Add products with no offers")
-      val items = Array("apple","orange","orange");
+      val items = Array("apple", "orange", "orange");
 
       Then("Total sum should be simple sum of prices")
-      shoppingCart.checkout(items) should be (1.10)
+      shoppingCart.checkout(items) should be(1.10)
     }
-  }
+  } //End of Feature with no offers
+
+    feature ("Shopping Cart with Offers"){
+
+      scenario("Testing Checkout BOGOF offer"){
+
+        Given("A shopping cart")
+        val shoppingCart =  ShoppingCart
+
+        When("2 apples added to the basket")
+        val items = Array("apple","apple");
+
+        Then("Total price should be same as one apple")
+        shoppingCart.checkout(items) should be (0.60)
+      }
+
+      scenario("Testing Checkout 3for2 offer"){
+
+        Given("A shopping cart")
+        val shoppingCart =  ShoppingCart
+
+        When("3 oranges added to the basket")
+        val items = Array("orange","orange","orange");
+
+        Then("Total price should equal to prices of 2 oranges")
+        shoppingCart.checkout(items) should be (0.50)
+      }
+
+      scenario("Testing Checkout both 3for2 and BOGOF offers"){
+
+        Given("A shopping cart")
+        val shoppingCart =  ShoppingCart
+
+        When("3 oranges and 2 apples added to the basket")
+        val items = Array("orange","orange","orange","apple","apple");
+
+        Then("Total price should equal to prices of 2 oranges and 1 apple")
+        shoppingCart.checkout(items) should be (1.10)
+      }
+
+
+      scenario("Testing Checkout with no offer"){
+
+        Given("A shopping cart")
+        val shoppingCart =  ShoppingCart
+
+        When("2 oranges and 1 apple added to the basket")
+        val items = Array("orange","orange","apple");
+
+        Then("Total price should equal to prices of 2 oranges and 1 apple")
+        shoppingCart.checkout(items) should be (1.10)
+      }
+
+    } //End of Feature with Offers
 
 }
